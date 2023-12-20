05b4ee2a18632f64b19d90f9245ce604a7ca6c8e
Vladislav Dubolear-Odobescu <vladislav.dubolear@mekanys.com>
2023-12-21 00:25:29 +0200

    add dynamic link, apply DRY concept, and stat use template tags and variable in html

--- products/views.py
+++ products/views.py
@@ -1,3 +1,39 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def index(request):
+    context = {
+        'title': 'Test Title',
+        'is_promotion': True,
+        }
+    return render(request, 'products/index.html', context)
+
+
+def products(request):
+    context = {
+        'title': 'Store - Каталог',
+        'products': [
+            {
+                'image': "/static/vendor/img/products/Adidas-hoodie.png",
+                'name': "Худи черного цвета с монограммами adidas Originals",
+                'price': 6090,
+                'description': 'Мягкая ткань для свитшотов. Стиль и комфорт – это образ жизни.',
+            },
+            {
+                'image': "/static/vendor/img/products/Blue-jacket-The-North-Face.png",
+                'name': "Синяя куртка The North Face",
+                'price': 23725,
+                'description': 'Гладкая ткань. Водонепроницаемое покрытие. Легкий и теплый пуховый наполнитель.',
+            },
+            {
+                'image': "/static/vendor/img/products/Brown-sports-oversized-top-ASOS-DESIGN.png",
+                'name': "Коричневый спортивный oversized-топ ASOS DESIGN",
+                'price': 3390,
+                'description': 'Материал с плюшевой текстурой. Удобный и мягкий.',
+            },
+        ]
+
+    }
+    return render(request, 'products/products.html', context)
