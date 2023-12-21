ff129381d13a1f84d04241415e1d551fc01c18f2
Vladislav Dubolear-Odobescu <vladislav.dubolear@mekanys.com>
2023-12-21 14:00:30 +0200

    add dynamic html data from DB

--- products/models.py
+++ products/models.py
@@ -6,6 +6,10 @@ class ProductCategory(models.Model):
     description = models.TextField(null=True, blank=True)
 
 
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=256)
     description = models.TextField()
@@ -13,3 +17,7 @@ class Product(models.Model):
     quantity = models.PositiveIntegerField(default=0)
     image = models.ImageField(upload_to='products_images')
     category = models.ForeignKey(ProductCategory, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return f'Продукт: {self.name} (Категория: {self.category.name})'
